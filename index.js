e3e203d588e092e8203aac30c7272d950d2e2f0f
moudud <sihamislam18@gmail.com>
2025-08-28 01:17:54 +0600

    for the next project deploy

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware setup
 const corsOptions = {
-  origin: [
-    'http://localhost:5173',
-    'https://greenplantproject-cae48.web.app' 
-  ],
+ origin: ["https://tree-plantnet-server-side-project.vercel.app/"],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
